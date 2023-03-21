d981e9e1b8da8bbbea65ff76fd8194f805303a26
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-03-21 22:16:28 +0900

    [Bronze V] Title: 사분면 고르기, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int x = sc.nextInt()
+			, y = sc.nextInt()
+			, quadrant = 0;
+			
+			if( x > 0 && y > 0 ) {
+				quadrant = 1;
+			}else if( x < 0 && y > 0 ) {
+				quadrant = 2;
+			}else if( x < 0 && y < 0 ) {
+				quadrant = 3;
+			}else if( x > 0 && y < 0 ) {
+				quadrant = 4;
+			}
+			System.out.println(quadrant);
+		}
+	}
+}
