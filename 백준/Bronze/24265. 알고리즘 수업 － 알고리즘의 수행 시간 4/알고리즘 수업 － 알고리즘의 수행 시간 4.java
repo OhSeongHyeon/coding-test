baf16b66acd2329886e3d0a4c39fcf9f93f58f7a
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-04-21 00:30:05 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	// 백준 
+    public static void main(String[] args) {
+        try(
+    		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+		){
+        	long n = Long.parseLong(br.readLine());
+            StringBuilder sb = new StringBuilder();
+            sb.append(n*(n-1)/2)
+            	.append("\n")
+            	.append(2);
+            bw.write(sb.toString());
+        	bw.flush();
+        }catch(Exception e) {
+        	e.printStackTrace();
+        }
+    }
+}
