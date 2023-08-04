963a648343c0086495cf87c3c4d26d6944b29761
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-08-05 00:34:34 +0900

    [unrated] Title: 리스트 자르기, Time: 0.03 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+		List<Integer> list = new ArrayList<>();
+		int a = slicer[0];
+		int b = slicer[1];
+		int c = slicer[2];
+		
+		switch (n) {
+		case 1:
+			for (int i = 0; i <= b; i++) {
+				list.add(num_list[i]);
+			}
+			break;
+		case 2:
+			for (int i = a; i < num_list.length; i++) {
+				list.add(num_list[i]);
+			}
+			break;
+		case 3:
+			for (int i = a; i <= b; i++) {
+				list.add(num_list[i]);
+			}
+			break;
+		case 4:
+			for (int i = a; i <= b; i+=c) {
+				list.add(num_list[i]);
+			}
+			break;
+		}
+		
+		int len = list.size();
+		int[] res = new int[len];
+		
+		for (int i = 0; i < len; i++) {
+			res[i] = list.get(i);
+		}
+		
+		return res;
+	}
+}
