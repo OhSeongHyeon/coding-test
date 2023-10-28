5b4cbaff2a0f2df0dfe8586c84dc995955919e33
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-10-29 01:28:27 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        return lcm(n, 6) / 6;
+    }
+	
+	private int lcm(int a, int b) {
+		return a*b/gcd(a, b);
+	}
+	
+	private int gcd(int a, int b) {
+		if(b==0) return a;
+		return gcd(b, a%b);
+	}
+}
