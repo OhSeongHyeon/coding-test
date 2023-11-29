aa2ffe6f86784187d8c46c05550f7ccfda6d9a06
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-11-30 00:27:09 +0900

    [Bronze V] Title: CASIO, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] split = br.readLine().split("\\s");
+		long a = Long.parseLong(split[0]);
+		long b = Long.parseLong(split[1]);
+		if(a==b) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+    }
+}
