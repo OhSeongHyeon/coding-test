c056993d83b00b59b1f2c502cc186ae36596e62f
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-11-30 00:39:33 +0900

    [Bronze V] Title: Rook, Time: 112 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] pattern = {
+            "  ___  ___  ___",
+            "  | |__| |__| |",
+            "  |           |",
+            "   \\_________/",
+            "    \\_______/",
+            "     |     |",
+            "     |     |",
+            "     |     |",
+            "     |     |",
+            "     |_____|",
+            "  __/       \\__",
+            " /             \\",
+            "/_______________\\"
+        };
+
+        for (String line : pattern) {
+            System.out.println(line);
+        }
+    }
+}
