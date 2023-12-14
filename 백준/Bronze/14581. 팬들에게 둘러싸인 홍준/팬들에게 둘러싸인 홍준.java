804086efc81a4a4992f8e3bd3d7599dfbf9a8444
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-12-14 23:54:11 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 132 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.printf(":fan::fan::fan:\n:fan::%s::fan:\n:fan::fan::fan:", br.readLine());
+    }
+
+}
