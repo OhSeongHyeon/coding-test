7cd36a5a00722accf524138df24a975676d3f014
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-12-22 09:56:18 +0900

    [Bronze IV] Title: 한글 2, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		System.out.println(new BufferedReader(new InputStreamReader(System.in)).readLine().charAt(0) - '가' + 1);
+	}
+}
