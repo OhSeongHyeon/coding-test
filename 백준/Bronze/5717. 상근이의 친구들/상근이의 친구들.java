ac17938c0214d831470993f81e55e54b36770962
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2023-12-29 22:43:24 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 120 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a==0 && b==0) {
+				System.out.println(sb);
+				return;
+			}
+			
+			sb.append(a+b).append("\n");
+		}
+	}
+
+}
