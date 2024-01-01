605ca42bab5e9a1402184200f49e9f2a02bc2cb4
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2024-01-02 01:29:14 +0900

    [Bronze IV] Title: 빵, Time: 136 ms, Memory: 14352 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.java
+++ 백준/Bronze/25377. 빵/빵.java
@@ -6,23 +6,18 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
-		int res = -1;
 		int min = Integer.MAX_VALUE;
 		
 		for (int i = 0; i < t; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			if(a > b) {
-				continue;
-			}
-			if(min > b - a) {
-				min = b - a;
-				res = b;
+			if(a <= b) {
+				min = Math.min(min, b);
 			}
 		}
 		
-		System.out.println(res);
+		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
 	}
 
 }
