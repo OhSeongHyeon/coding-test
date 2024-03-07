873dcd5b9a4dde489956c41fddc23a37105087b5
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2024-03-08 08:55:17 +0900

    [Bronze III] Title: A/B - 3, Time: 264 ms, Memory: 17680 KB, Score: 630 point -BaekjoonHub

--- 백준/Bronze/16428. A／B － 3/A／B － 3.java
+++ 백준/Bronze/16428. A／B － 3/A／B － 3.java
@@ -8,7 +8,17 @@ public class Main {
 		String[] sp = br.readLine().split("\\s");
 		BigInteger a = new BigInteger(sp[0]);
 		BigInteger b = new BigInteger(sp[1]);
-		System.out.printf("%s\n%s", a.divide(b), a.remainder(b));
+		final BigInteger zero = BigInteger.ZERO;
+		final BigInteger one = BigInteger.ONE;
+		
+		if(a.compareTo(zero) < 0 && b.compareTo(zero) < 0) {
+			System.out.printf("%s\n%s", a.divide(b).add(one), a.remainder(b).subtract(b));
+		}else if(a.compareTo(zero) < 0 && b.compareTo(zero) > 0) {
+			System.out.printf("%s\n%s", a.divide(b).subtract(one), a.remainder(b).add(b));
+		}else {
+			System.out.printf("%s\n%s", a.divide(b), a.remainder(b));
+		}
+		
 	}
 
 }
