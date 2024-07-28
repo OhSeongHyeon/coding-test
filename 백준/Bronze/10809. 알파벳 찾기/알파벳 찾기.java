1c0e502a757201b01c2d5950e0ccb9638b9baa15
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2024-07-29 01:21:30 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,34 +1,30 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		try(
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
-		){
-			String[] readLine = br.readLine().split("");
-			int[] firstTimeLocation = new int[26];
-			Arrays.fill(firstTimeLocation, -1);
-			
-			for (int i = 0; i < readLine.length; i++) {
-				char c = readLine[i].charAt(0);
-				if (firstTimeLocation[c - 'a'] == -1) {
-					firstTimeLocation[c - 'a'] = i;
-				}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inp = br.readLine();
+		int inpLen = inp.length();
+		int[] alpha = new int['z'-'a' + 1];
+		Arrays.fill(alpha, -1);
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < inpLen; i++) {
+			int alphaIdx = inp.charAt(i) - 'a';
+			if(alpha[alphaIdx] == -1) {
+				alpha[alphaIdx] = i;
 			}
-			
-			String result = Arrays.toString(firstTimeLocation).replaceAll("[\\[\\],]", "");
-			bw.write(result);
-			bw.flush();
-		}catch(IOException ioe) {
-			ioe.printStackTrace();
 		}
+		
+		for (int i = 0; i < alpha.length; i++) {
+			sb.append(alpha[i])
+				.append(" ");
+		}
+		
+		sb.setLength(sb.length() - 1);
+		System.out.println(sb);
 	}
+	
 }
