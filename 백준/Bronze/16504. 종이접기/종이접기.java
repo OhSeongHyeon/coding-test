29afc30418ba63ee1dc9dc63604b0f9dc0d52480
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2024-07-31 01:05:17 +0900

    [Bronze III] Title: 종이접기, Time: 688 ms, Memory: 108964 KB -BaekjoonHub

--- 백준/Bronze/16504. 종이접기/종이접기.java
+++ 백준/Bronze/16504. 종이접기/종이접기.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
@@ -6,11 +5,15 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		long sum = br.lines()
-			.limit(n)
-			.flatMap(l -> Arrays.stream(l.split(" ")))
-			.mapToLong(Long::parseLong)
-			.sum();
+		long sum = 0;
+		
+		for (int i = 0; i < n; i++) {
+			String[] sp = br.readLine().split(" ");
+			for (int j = 0; j < n; j++) {
+				sum += Long.parseLong(sp[j]);
+			}
+		}
+		
 		System.out.println(sum);
 	}
 	
