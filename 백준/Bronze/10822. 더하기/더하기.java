ff335f822858b9ed4cb86647c88e023c1330767b
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2024-09-05 00:22:00 +0900

    [Bronze II] Title: 더하기, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10822. 더하기/더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = Arrays.stream(br.readLine().split(","))
+			.mapToInt(Integer::parseInt)
+			.sum();
+		System.out.println(sum);
+	}
+	
+}
