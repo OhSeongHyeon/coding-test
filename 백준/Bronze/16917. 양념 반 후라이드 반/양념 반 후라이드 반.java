7773a38599c41ad6b905b25469af3a3c752f1c4f
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2024-09-06 13:29:59 +0900

    [Bronze II] Title: 양념 반 후라이드 반, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/16917. 양념 반 후라이드 반/양념 반 후라이드 반.java
+++ 백준/Bronze/16917. 양념 반 후라이드 반/양념 반 후라이드 반.java
@@ -10,11 +10,14 @@ public class Main {
 		int ba = Integer.parseInt(sp[2]);
 		int x = Integer.parseInt(sp[3]);
 		int y = Integer.parseInt(sp[4]);
+		int ba2 = ba * 2;
 		int cost = 0;
 		
-		if(ba * 2 <= ya + hu) {
-			cost += ba * 2 * (x < y ? x : y);
-			cost += x > y ? Math.min(ya * (x-y), ba * 2 * (x-y)) : Math.min(hu * (y-x), ba * 2 * (y-x));
+		if(ba2 <= ya + hu) {
+			cost += ba2 * Math.min(x, y);
+			int remain = Math.abs(x-y);
+			int ba2Cost = ba2 * remain;
+			cost += x > y ? Math.min(ya * remain, ba2Cost) : Math.min(hu * remain, ba2Cost);
 		}else {
 			cost += ya * x + hu * y;
 		}
