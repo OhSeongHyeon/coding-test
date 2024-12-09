3b88fd0fe06a2b426846edb48168611343af3a22
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2024-12-10 00:30:33 +0900

    [Bronze V] Title: UOS 문자열, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/32929. UOS 문자열/UOS 문자열.java
+++ 백준/Bronze/32929. UOS 문자열/UOS 문자열.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		System.out.println("UOS".charAt(n%3 == 0 ? 2 : n%3 - 1));
+		String s = "UOS";
+		int len = s.length();
+		System.out.println(s.charAt(n%len == 0 ? len-1 : n%len - 1));
 	}
 
 }
