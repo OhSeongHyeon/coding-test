747a8ed521b664b7747c21ac623534f7d0e57f66
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2024-12-20 04:32:11 +0900

    [Bronze V] Title: 余り (Remainder), Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int remainder = X % 21;
+        System.out.println(remainder);
+    }
+}
