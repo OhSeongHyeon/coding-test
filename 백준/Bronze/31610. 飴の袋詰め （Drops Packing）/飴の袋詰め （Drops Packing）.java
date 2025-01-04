d3f7928a4027c2e0ad89eecf456b07827804b5ef
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2025-01-05 00:18:35 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int totalCost = A * B + C;
+        System.out.println(totalCost);
+    }
+}
