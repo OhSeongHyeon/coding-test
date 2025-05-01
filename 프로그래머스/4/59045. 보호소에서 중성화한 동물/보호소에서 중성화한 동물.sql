556dbdc57a98262def58d8a93e95548b15943153
OhSeongHyeon <79860377+OhSeongHyeon@users.noreply.github.com>
2025-05-02 02:46:21 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,41 @@
+# SELECT
+#     *
+# FROM
+#     ANIMAL_INS
+# WHERE 1=1
+# AND (SEX_UPON_INTAKE NOT LIKE 'Spayed%'
+# AND  SEX_UPON_INTAKE NOT LIKE 'Neutered%')
+
+# SELECT
+#     *
+# FROM
+#     ANIMAL_OUTS
+# WHERE 1=1
+# AND SEX_UPON_OUTCOME LIKE 'Spayed%'
+# OR  SEX_UPON_OUTCOME LIKE 'Neutered%'
+
+SELECT
+    O.ANIMAL_ID
+,   O.ANIMAL_TYPE
+,   O.`NAME`
+FROM (
+    SELECT
+        ANIMAL_ID
+    FROM
+        ANIMAL_INS
+    WHERE
+        SEX_UPON_INTAKE LIKE 'Intact%'
+) AS I
+INNER JOIN (
+    SELECT
+        ANIMAL_ID
+    ,   ANIMAL_TYPE
+    ,   `NAME`
+    FROM
+        ANIMAL_OUTS
+    WHERE
+        SEX_UPON_OUTCOME LIKE 'Spayed%' OR SEX_UPON_OUTCOME LIKE 'Neutered%'
+) AS O
+ON
+    I.ANIMAL_ID = O.ANIMAL_ID
+
